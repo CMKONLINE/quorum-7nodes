1f3b8248910c893e35c8d9821b99177fdbe7e2e9
Rus Hughes <russell.hughes@gmail.com>
2017-08-12 22:36:39 +0700

    added other nodes to truffle.js

--- truffle.js
+++ truffle.js
@@ -4,6 +4,16 @@ module.exports = {
       host: "localhost",
       port: 22000, // was 8545
       network_id: "*" // Match any network id
+    },
+    nodefour:  {
+      host: "localhost",
+      port: 22003,
+      network_id: "*" // Match any network id
+    },
+    nodeseven:  {
+      host: "localhost",
+      port: 22006,
+      network_id: "*" // Match any network id
     }
   }
 };
